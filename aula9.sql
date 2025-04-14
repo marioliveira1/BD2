efc8f62ddf50e147d0408c30b08b15cee0e39e15
Marillisy <Marioliveiraaa12@gmail.com>
2025-04-14 15:45:44 -0300

    aula 9

---
+++ aula9.sql
@@ -0,0 +1,18 @@
+use cadastro;
+-- Sublinguagem DDl
+desc curso;
+desc estudante;
+desc estudantecursa;
+
+
+-- DQL - comandos de consulta
+select * from curso;
+select nome,descricao from curso;
+select nome,descricao from curso where nome like "S%";
+select count(nome) from estudante;
+
+-- Consulta envolvendo 2 ou mais tabelas
+select estudante.nome, curso.nome from estudante left join
+estudantecursa on estudante.id = estudantecursa.idEstudante
+join curso on estudantecursa.idcurso = curso.idCurso;
+
